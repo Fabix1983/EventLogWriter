b8bf919c628948cbcc764273ad98549c9715b259
Fabio Gianlorenzo <gianlorenzof@libero.it>
2024-01-18 19:31:37 +0100

    first release

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Data;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace EventLogWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INIZIO");
+
+            string Testo = "";
+            string AppName = "My C# console APP per scrittura nell'event Log di windows";
+
+            try
+            {
+                Testo = "Questo è un log event di tipo informazione";
+                EventLogWriter(1, AppName + " - " + Testo);
+
+                Testo = "Questo è un log event di tipo warning";
+                EventLogWriter(2, AppName + " - " + Testo);
+
+                Testo = "Questo è un log event di tipo errore";
+                EventLogWriter(3, AppName + " - " + Testo);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Errore catch " + ex.Message.ToString());
+            }
+
+            Console.WriteLine("FINE");
+            Console.ReadLine();
+        }
+
+        public static void EventLogWriter(int type, string Testo)
+        {
+            using (EventLog eventLog = new EventLog("Application"))
+            {
+                eventLog.Source = "Application";
+
+                if (type == 1) // Information
+                {
+                    eventLog.WriteEntry(Testo, EventLogEntryType.Information);
+                }
+
+                if (type == 2) // Warning
+                {
+                    eventLog.WriteEntry(Testo, EventLogEntryType.Warning);
+                }
+
+                if (type == 3) // Error
+                {
+                    eventLog.WriteEntry(Testo, EventLogEntryType.Error);
+                }
+            }
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# EventLogWriter
-C# Console Application for insert into the Windows Event Log.
+Semplice Applicazione Console C# di esempio per l'inserimento di una voce nel Event Log di Windows nella sezione Applicazione.
+
+Licenza di rilascio: MIT
+
+Simple C# Console Application with example of entry insert into the Windows Event Log in the Application section.
+
+Release license: MIT
+
+Gianlorenzo Fabio
